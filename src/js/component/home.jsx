c8d21b5613cdb674b77e34e58b9662fbefd1b9f2
DVG-Dexter <161875360+DVG-Dexter@users.noreply.github.com>
2024-11-02 17:13:41 +0000

    casilisto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,134 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [newEntry, setNewEntry] = useState('');
+    const [taskList, setNewList] = useState([]);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                "label": newEntry,
+                "is_done": false
+            })
+        };
+        fetch("https://playground.4geeks.com/todo/todos/diegovega", requestOptions)
+            .then((response) => response.json())
+            .then((result) => {
+                getTasks()
+                console.log(result)
+                setNewEntry("")
+            })
+            .catch((error) => console.error(error));
+    }
+
+    const createUser = () => {
+        const requestOptions = {
+            method: "POST",
+            redirect: "follow"
+        };
+
+        fetch("https://playground.4geeks.com/todo/users/diegovega", requestOptions)
+            .then((response) => response.json())
+            .then((result) => {
+                getTasks()
+                console.log(result)
+            })
+            .catch((error) => console.error(error));
+    }
+
+    const getTasks = () => {
+        const requestOptions = {
+            method: "GET",
+            redirect: "follow"
+        };
+
+        fetch("https://playground.4geeks.com/todo/users/diegovega", requestOptions)
+            .then((response) => {
+                console.log(response);
+                if (response.ok === false) {
+                    createUser()
+                } else { return response.json() }
+            })
+            .then((result) => setNewList(result.todos))
+            .catch((error) => console.error(error));
+    }
+
+    const deleteTask = (id) => {
+        const requestOptions = {
+            method: "DELETE",
+            redirect: "follow"
+        };
+
+        fetch("https://playground.4geeks.com/todo/todos/" + id, requestOptions)
+            .then((response) => response.text())
+            .then((result) => {
+                getTasks()
+                console.log(result)
+            })
+            .catch((error) => console.error(error));
+    }
+
+    const deleteAllTasks = () => {
+        const requestOptions = {
+            method: "DELETE",
+            redirect: "follow"
+        };
+
+        fetch("https://playground.4geeks.com/todo/users/diegovega", requestOptions)
+            .then((response) => response.text())
+            .then((result) => {
+                console.log(result)
+                setNewList([]);  
+                createUser();
+            })
+            .catch((error) => console.error(error));
+    }
+
+    useEffect(() => {
+        getTasks()
+    }, [])
+
+    return (
+        <div className="container w-50">
+            <h1 className="text-center">Lista de Tareas</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail" className="from-label"></label>
+                    <input
+                        className="form-control text-left"
+                        placeholder="Escriba aqui su tarea..."
+                        onChange={(event) => setNewEntry(event.target.value)}
+                        onKeyDown={(event) => {
+                            if (event.key === 'Enter') {
+                                handleSubmit(event)
+                            }
+                        }}
+                        value={newEntry}
+                    />
+                </div>
+            </form>
+            <h2 style={{ color: 'white' }}>Tareas Pendientes:</h2>
+            <ul className="todos">{
+                    taskList.map((task, index) => {
+                        return (
+                            <li key={index} style={{ color: 'white', fontSize: '1.5em' }}>
+								<span style={{ fontStyle: 'italic'}}>{task.label}</span>{'    '}
+                                <i 
+                                    onClick={() => { deleteTask(task.id)}}
+                                    className="fa-solid fa-trash">
+                                </i>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            <button onClick={deleteAllTasks} className="btn btn-danger mb-3">Borrar todas entradas</button>
+            <p style={{ color: 'white' }}>{taskList.length} Pendientes</p>
+        </div>
+    );
 };
 
 export default Home;
